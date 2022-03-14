2f08a3bd98e556b2512421628dd10c3f99962a53
Alain Plattner <alainplattner@gmail.com>
2022-03-14 13:02:59 -0500

    changed indices locations in sph coef

--- plotCorrelation.sh
+++ plotCorrelation.sh
@@ -11,8 +11,9 @@ gmt begin figures/${1} pdf
 gmt set FONT 9
 
 gmt makecpt -Cvik -T-1/1/0.25 -Z
-gmt grdimage correlation/${1}.grd -JX7c/-7c -R0.5/${cells}/0.5/${cells} -C -Bpxccorrelation/labels.txt -Bpx+a-30 -Bpyccorrelation/labels.txt
+gmt grdimage correlation/${1}.grd -JX7c/-7c -R0.5/${cells}/0.5/${cells} -C -Bpxccorrelation/labelsUD.txt -Bpyccorrelation/labelsUD.txt
 #-Balabels.txt
+#-Bpx+a-30
 
 gmt colorbar -DJRC+o3.8c/0c+w7c -B0.5+l"correlation coefficient"
 #+o-0.5c/0c
