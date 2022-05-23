ddfb8ee9e78aa0c39de3fc506f7eff44d88af455
Alain Plattner <alainplattner@gmail.com>
2022-05-23 17:53:56 -0500

    just added a plot

--- showManyg10.m
+++ showManyg10.m
@@ -16,5 +16,15 @@ function showManyg10(grng,Lmax)
     rsvalues(i) = rs;
     
   end
-    
-keyboard
+
+  % For plotting, turn g10 values from Kivelson to Schmidt seminormalized
+  sfc = (-1).^(0)./(sqrt((2*1+1)/(4*pi)));
+  grng = grng * 2 / sfc;
+  
+  scatter(grng,rsvalues,[],rmsvalues,'filled');
+  c=colorbar;
+  xlabel('g_{10}');
+  ylabel('source radius [km]');
+  ylabel(c,'rmse [nT]')
+  print(sprintf('Lmax%d_manyg10.pdf',Lmax)  ,'-dpdf', '-bestfit')
+ 
