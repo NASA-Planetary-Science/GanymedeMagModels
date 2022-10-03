aa569a472b37e3dd0229e499b997ff372d59c88d
Alain Plattner <alainplattner@gmail.com>
2022-10-03 17:00:28 -0500

    working on rmse for induced inversions

---
+++ getRMSind.m
@@ -0,0 +1,65 @@
+function rmsval = getRMSind(coefs,Lmax,tracks,indname)
+% Same as getRMS, but also subtacts the induced field.
+  
+  rplanet = 2631.2;
+  fact=-2;
+  cf = coefs(1:end-3*length(tracks))*rplanet;
+  UFvals = coefs(length(cf)+1:end);
+
+  % The sum of the squares of differences
+  sumsq = nan(size(tracks));
+
+  % The number of data points along each rack
+  numdat = nan(size(tracks));
+  
+  
+  for i = 1:length(tracks)
+
+    [starttime,endtime,Bbgx,Bbgy,Bbgz] = getTimeJup(tracks(i));
+    if tracks(i) < 100
+      filename = sprintf('GalileoData/ORB%02d_GAN_IAU.txt',tracks(i));
+      [data,time] = importData(filename);
+    else
+      filename = sprintf('JunoData/ORB%03d_GAN_IAU.TAB',tracks(i));
+      [data,time] = importJunoData(filename);
+    end
+    index = time >= starttime & time <= endtime;
+    time = time(index);
+
+    [Bx,By,Bz,X,Y,Z] = prepData(tracks(i),rplanet);
+
+    % Calculate and then subtract the calculated induced field
+    [Bindx,Bindy,Bindz] = getInduced(tracks(i),indname,starttime,endtime);
+
+    % Now subtract this induced field from the data
+    Bx = Bx - Bindx;
+    By = By - Bindy;
+    Bz = Bz - Bindz;
+
+    
+    % Evaluate data from model
+    rGcart = evalSpHarm(X, Y, Z, rplanet, Lmax, fact, true((Lmax+1)^2-1, 1));
+
+    dcalc = rGcart'*cf;
+
+    % Add the uniform field back in
+    ind = find(tracks==tracks(i));
+
+    UBX = UFvals(3*(ind-1)+1);
+    UBY = UFvals(3*(ind-1)+2);
+    UBZ = UFvals(3*(ind-1)+3);
+
+    BXsim = dcalc(1:length(X)) + UBX;
+    BYsim = dcalc(length(X)+1:2*length(X)) + UBY;
+    BZsim = dcalc(2*length(X)+1:end) + UBZ;
+
+    sumsq(i) = sum( (Bx-BXsim).^2 + (By-BYsim).^2 + (Bz-BZsim).^2  );
+    numdat(i) = 3*length(BXsim);
+
+    %keyboard
+    
+  end
+
+  %rmsval = sqrt(sum(sumsq)/sum(numdat));
+
+  rmsval = rms(sqrt(sumsq./numdat));
--- plotSplitCoefs.sh
+++ plotSplitCoefs.sh
@@ -1,16 +1,21 @@
 gmt begin figures/splitCoefs pdf
 
-gmt set FONT 9
+gmt set FONT 12
 
 #range=-850/-650
 #inc=50
 range=-850/850
-inc=200
+inc=400
 
 gmt basemap -R-0.5/0.5/${range} -JX1c/4c -Bxa1 -Bya${inc} -By+l"coefficient [nT]" -BneSW -Bpxccoefs/coeflabels.txt 
-gmt plot coefs/split_L1m0.txt -Sp4p
+gmt plot coefs/split_ind_L1m0.txt -Sc4p -G150
+gmt plot coefs/split_L1m0.txt -Ss4p -W1p
+
+
 
 gmt basemap -X1.5c -R0.5/14.5/-100/100 -JX14c/4c -Bxa1 -Bya50 -By+l"coefficient [nT]" -BnESw -Bpxccoefs/coeflabels.txt 
-gmt plot coefs/split_Lrest.txt -Sp4p
+gmt plot coefs/split_ind_Lrest.txt -Sc4p -G150
+gmt plot coefs/split_Lrest.txt -Ss4p  -W1p
+
 
 gmt end show
