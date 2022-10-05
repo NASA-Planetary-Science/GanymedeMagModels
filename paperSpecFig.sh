bff0f358a1639f82fcc3a1587f60ccab406df49f
Alain Plattner <alainplattner@gmail.com>
2022-10-05 17:50:04 -0500

    working on spec figure

--- paperSpecFig.sh
+++ paperSpecFig.sh
@@ -2,31 +2,64 @@
 
 gmt begin figures/paperSpecFig pdf
 
-lt=1p
-sym=5p
-colfit=120/120/120
+
 gmt set FONT 14
 
-gmt basemap -R0.8/3.2/2e3/3e6 -JX10c/5cl  -Bxa1 -Bya10p -Bx+l"spherical-harmonic degree @[l@[" -By+l"power [nT@+2@+]"
+gmt basemap -R0.8/3.2/1e3/3e6 -JX10c/3cl  -Bxa1 -Bya10p -Bx+l"spherical-harmonic degree @[l@[" -By+l"power [nT@+2@+]"
 
-#gmt plot specs/KivelsonEtAl2002_spec_rs_229km.txt -W${lt}
-#gmt plot specs/KivelsonEtAl2002_spec_rs_229km.txt -Sc${sym} -W1p -l"K 2002"
 
-#gmt plot specs/Lmax1_spec_rs_210km.txt -Sc${sym} -W1p  -l"@%2%L@%%@-max@- = 1"
+lt=1p
+lcol=150/150/150
+sym=c4p
+scol=150/150/150
 
-gmt plot specs/Lmax2_spec_rs_182km.txt -W${lt}
-gmt plot specs/Lmax2_spec_rs_182km.txt -Ss${sym} -W1p -l"@%2%L@%%@-max@- = 2"
 
-gmt plot specs/Lmax3_spec_rs_966km.txt -W${lt}
-gmt plot specs/Lmax3_spec_rs_966km.txt -St${sym} -W1p  -l"@%2%L@%%@-max@- = 3"
-gmt plot specs/Lmax3_spec_rs_966km_fittedMcLeod.txt -W${lt},${colfit},-
+gmt plot specs/Lmax1_indhigh_spec_rs_200km.txt -S${sym} -G${scol}
 
-gmt plot specs/Lmax3_g10-900_spec_rs_1488km.txt -W${lt} 
-gmt plot specs/Lmax3_g10-900_spec_rs_1488km.txt -Sa${sym} -W1p  -l"@%2%g@%%@-1@-@+0@+ = -900"
-gmt plot specs/Lmax3_g10-900_spec_rs_1488km_fittedMcLeod.txt -W${lt},${colfit},-
+gmt plot specs/Lmax2_indhigh_spec_rs_138km.txt -W${lt},${lcol}
+gmt plot specs/Lmax2_indhigh_spec_rs_138km.txt -S${sym} -G${scol}
 
-#-l"@%2%g@%%@-1@-@+0@+ = -900"
+gmt plot specs/Lmax3_indhigh_spec_rs_1077km.txt -W${lt},${lcol}
+gmt plot specs/Lmax3_indhigh_spec_rs_1077km.txt -S${sym} -G${scol} 
 
-gmt legend -DjBL #--FONT=13
+
+
+gmt plot specs/Lmax1_indlow_spec_rs_210km.txt -S${sym} -G${scol} 
+
+gmt plot specs/Lmax2_indlow_spec_rs_137km.txt -W${lt},${lcol}
+gmt plot specs/Lmax2_indlow_spec_rs_137km.txt -S${sym} -G${scol} 
+
+gmt plot specs/Lmax3_indlow_spec_rs_1059km.txt -W${lt},${lcol}
+gmt plot specs/Lmax3_indlow_spec_rs_1059km.txt -S${sym} -G${scol}
+
+
+
+
+gmt plot specs/Lmax1_indsimple_spec_rs_210km.txt -S${sym} -G${scol}
+
+gmt plot specs/Lmax2_indsimple_spec_rs_137km.txt -W${lt},${lcol}
+gmt plot specs/Lmax2_indsimple_spec_rs_137km.txt -S${sym} -G${scol}
+
+gmt plot specs/Lmax3_indsimple_spec_rs_1058km.txt -W${lt},${lcol}
+gmt plot specs/Lmax3_indsimple_spec_rs_1058km.txt -S${sym} -G${scol}
+
+
+
+
+lt=1p
+lcol=black
+sym=s4p
+
+
+gmt plot specs/Lmax1_spec_rs_210km.txt -S${sym} -W1p
+
+gmt plot specs/Lmax2_spec_rs_182km.txt -W${lt},${lcol}
+gmt plot specs/Lmax2_spec_rs_182km.txt -S${sym}  -W1p
+
+gmt plot specs/Lmax3_spec_rs_966km.txt -W${lt},${lcol}
+gmt plot specs/Lmax3_spec_rs_966km.txt -S${sym}  -W1p
+
+
+
 
 gmt end show
