f445965f02c75d20e9ba6c92f6c15d965d074a73
Alain Plattner <alainplattner@gmail.com>
2022-12-13 13:21:53 -0600

    made some figures for AGU 2022

--- plotFieldsTracksTalk.sh
+++ plotFieldsTracksTalk.sh
@@ -14,8 +14,9 @@ gmt set FONT 16,white
 gmt makecpt -Cvik -T-${2}/${2}/${3} -Z
 gmt grdimage fields/${1}.nc -JW0/10c -Rd -C -Bg30
 
-col=green
+#col=green
 #col=150/150/150
+col=100/100/100
 
 gmt plot exportedTracks/Galileo1.txt -Sc0.1c -W${col}
 gmt plot exportedTracks/Galileo2.txt -Sc0.1c -W${col}
@@ -26,12 +27,12 @@ gmt plot exportedTracks/Galileo29.txt -Sc0.1c -W${col}
 
 gmt plot exportedTracks/Juno1.txt -Sc0.1c -W${col}
 
-gmt text --FONT=${col} <<EOF
--153 18 G1
--10 75 G2
-140 -17 G28
--102 15 J
-EOF
+# gmt text --FONT=${col} <<EOF
+# -153 18 G1
+# -10 75 G2
+# 140 -17 G28
+# -102 15 J
+# EOF
 
 
 # gmt text --FONT=${col} <<EOF
