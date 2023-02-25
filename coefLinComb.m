5a45ee8d7a52eb0ed0403e92ec5140fb5d298660
Alain Plattner <alainplattner@gmail.com>
2023-02-24 20:08:56 -0600

    done with calculation of lin combs. Next: Plot these lin combs

--- coefLinComb.m
+++ coefLinComb.m
@@ -1,12 +1,13 @@
 %function coefLinComb(coef1name,coef2name,fac1,outname)
-function coefLinComb(fac1,outname)
+%function coefs = coefLinComb(fac1,outname)
+function coefs = coefLinComb(fac1,tracks)
 
-  if fac1==1
-    Lmax=2;
-  else
-    Lmax=3;
-  end
-  tracks=[1,2,7,8,28,29,101];
+  % if fac1==1
+  Lmax=2;
+  % else
+  %   Lmax=3;
+  % end
+  %tracks=[1,2,7,8,28,29,101];
     
   if(fac1>1 | fac1<0)
     error('fac1 needs to be between 0 and 1')
@@ -14,41 +15,41 @@ function coefLinComb(fac1,outname)
 
 
   % Need to be same max sphharm degree
-  if fac1==1
-    cfinfo = load(fullfile('coefs','Lmax2_manycoefs'));
-    coefs = mean(cfinfo.cfmat,2);
+  % if fac1==1
+  %   cfinfo = load(fullfile('coefs','Lmax2_manycoefs'));
+  %   coefs = mean(cfinfo.cfmat,2);
     
-  %  coefs = coefs1;
-  else
-    %coefs1 = readmatrix(fullfile('coefs',[coef1name,'_coefs']));
-    %coefs2 = readmatrix(fullfile('coefs',[coef2name,'_coefs']));
-    cfinfo1 = load(fullfile('coefs','Lmax2_manycoefs'));
-    cf = mean(cfinfo1.cfmat,2);
-    % Transform this from Lmax=2 to Lmax=3 coefficients
-    Lnew=3; Lold=2;
-    coefs1=zeros( (Lnew+1)^2-1 + 3*length(tracks) ,1);
-    coefs1( 1:(Lold+1)^2-1 ) = cf( 1:(Lold+1)^2-1 );
-    coefs1( (Lnew+1)^2:end ) = cf( (Lold+1)^2:end );
-    clear cfinfo1,cf;
+  % %  coefs = coefs1;
+  % else
   
-    cfinfo2 = load(fullfile('coefs','Lmax3_manycoefs'));
+  %cfinfo1 = load(fullfile('coefs','Lmax2_manycoefs'));
+  cfinfo1 = load(fullfile('coefs','Lmax2_manycoefs'));
+    coefs1 = mean(cfinfo1.cfmat,2);
+    % % Transform this from Lmax=2 to Lmax=3 coefficients
+    % Lnew=3; Lold=2;
+    % coefs1=zeros( (Lnew+1)^2-1 + 3*length(tracks) ,1);
+    % coefs1( 1:(Lold+1)^2-1 ) = cf( 1:(Lold+1)^2-1 );
+    % coefs1( (Lnew+1)^2:end ) = cf( (Lold+1)^2:end );
+    % clear cfinfo1,cf;
+  
+    cfinfo2 = load(fullfile('coefs','Lmax2_B_i6_v-35_manycoefs'));
     coefs2 = mean(cfinfo2.cfmat,2);
 
     coefs = coefs1*fac1 + coefs2*(1-fac1);
-  end
+  %end
   
-  cf = coefs(1:(Lmax+1)^2-1);
+  %cf = coefs(1:(Lmax+1)^2-1);
 
-  writematrix(coefs,fullfile('coefs',[outname,'_coefs']))
+  %writematrix(coefs,fullfile('coefs',[outname,'_coefs']))
   
   % Export the spatial field for GMT to plot
-  exportThisField(cf,[outname,'_field'],0);
+  %exportThisField(cf,[outname,'_field'],0);
 
   % Write out data for data comparison in GMT plot
-  exportTracks = tracks; % You can change for which tracks you want to export
+  %exportTracks = tracks; % You can change for which tracks you want to export
   % This could be different from the tracks for which you calculated the field
-  exportSimData(coefs, tracks, [outname,'_simData'], exportTracks);
+  %exportSimData(coefs, tracks, [outname,'_simData'], exportTracks);
 
   % Write out the power spectrum and print the source depth
-  writeSpec(coefs(1:(Lmax+1)^2-1),[outname,'_spec'])
+  %writeSpec(coefs(1:(Lmax+1)^2-1),[outname,'_spec'])
   
--- manyCoefLinComb.m
+++ manyCoefLinComb.m
@@ -1,13 +1,18 @@
-function manyCoefLinComb(wht)
+function manyCoefLinComb()
   
   facs = 0:0.025:1;
   %facs = 1;
-  
-  if strcmp(wht,'doit')
+  Lmax = 2;
+  tracks=[1,2,7,8,28,29,101];
+  %if strcmp(wht,'doit')
+
+        % Condon Shortley
+        [~,~,~,~,~,~,bigm] = addmon(Lmax);
+        csphase = (-1).^bigm;
   
     for i=1:length(facs)
       
-      outname = sprintf('LinComb_fac%gpc',round(facs(i)*100));
+      %outname = sprintf('LinComb_fac%gpc',round(facs(i)*100));
       %if facs(i)==1
         % We only go to degree 2 if the factor for Lmax3 is zero
       %  coefLinComb('Lmax2','Lmax3',facs(i),outname);
@@ -15,45 +20,61 @@ function manyCoefLinComb(wht)
       %  coefLinComb('Lmax2exp','Lmax3',facs(i),outname);
       %end
       % Wrote it such that it is always between Lmax=2 and Lmax=3
-      coefLinComb(facs(i),outname);
+      coefs = coefLinComb(facs(i),tracks);
+
+     rmse = getRMS(coefs, Lmax, tracks);
+     rmsval(i) = rms(rmse(:)); 
+
+     % Get Q/D
+     fac = 2;
+     coefSch = [0;coefs(1:(Lmax+1)^2-1)]*fac;
+     coefSch = Simons2Olsen(coefSch);
+     coefSch = coefSch.*csphase;
+     D = 2*sum(coefSch(2:4).^2);
+     Q = 3*sum(coefSch(5:9).^2);
+     QD(i) = Q/D*(1/0.25)^2;
+
+ 
       
     end
 
-  elseif strcmp(wht,'showspecs')
+    writematrix([facs(:),QD(:),rmsval(:)],'linCombResults.txt')
 
-    rs = [965,954,942,930,917,905,892,879,866,853,840,826,812,797,783,768,753,737,721,705,688,671,654,635,617,597,577,557,535,512,489,464,438,409,379,346,310,268,219,155,182];
-    % I got these from reading of the files from the output of 'doit';
+  % elseif strcmp(wht,'showspecs')
+
+  %   rs = [965,954,942,930,917,905,892,879,866,853,840,826,812,797,783,768,753,737,721,705,688,671,654,635,617,597,577,557,535,512,489,464,438,409,379,346,310,268,219,155,182];
+  %   % I got these from reading of the files from the output of 'doit';
     
-    for i=1:length(facs)
-      specname = fullfile('specs',sprintf('LinComb_fac%gpc_spec_rs_%dkm.txt',round(facs(i)*100),rs(i)));
+  %   for i=1:length(facs)
+  %     specname = fullfile('specs',sprintf('LinComb_fac%gpc_spec_rs_%dkm.txt',round(facs(i)*100),rs(i)));
 
-      spec = load(specname);
+  %     spec = load(specname);
 
-      semilogy(spec(:,1),spec(:,2))
-      hold on
-    end
-    hold off
+  %     semilogy(spec(:,1),spec(:,2))
+  %     hold on
+  %   end
+  %   hold off
 
-  elseif strcmp(wht,'calcrmse')
+  % elseif strcmp(wht,'calcrmse')
 
-    rs = [965,954,942,930,917,905,892,879,866,853,840,826,812,797,783,768,753,737,721,705,688,671,654,635,617,597,577,557,535,512,489,464,438,409,379,346,310,268,219,155,182];
+  %   rs = [965,954,942,930,917,905,892,879,866,853,840,826,812,797,783,768,753,737,721,705,688,671,654,635,617,597,577,557,535,512,489,464,438,409,379,346,310,268,219,155,182];
   
-    % I got these from reading of the files from the output of 'doit';
+  %   % I got these from reading of the files from the output of 'doit';
     
-    for i=1:length(facs)
-      outname = sprintf('LinComb_fac%gpc',round(facs(i)*100));
-      rmse = getRMSE(outname);
-      rmsval(i) = rms(rmse(:));
-    end
-    writematrix([facs(:),rs(:),rmsval(:)],'linCombResults.txt')
+  %   for i=1:length(facs)
+  %     outname = sprintf('LinComb_fac%gpc',round(facs(i)*100));
+  %     rmse = getRMSE(outname);
+  %     rmsval(i) = rms(rmse(:));
+  %   end
+  %   writematrix([facs(:),rs(:),rmsval(:)],'linCombResults.txt')
 
-    scatter(facs, rs, [], rmsval,'filled')
-    c=colorbar;
-    xlabel('fraction of Lmax=2');
-    ylabel('source radius [km]');
-    ylabel(c,'rmse [nT]')
-    print('LinCombFig.pdf'  ,'-dpdf', '-bestfit')
+  %   scatter(facs, rs, [], rmsval,'filled')
+  %   c=colorbar;
+  %   xlabel('fraction of Lmax=2');
+  %   ylabel('source radius [km]');
+  %   ylabel(c,'rmse [nT]')
+  %   print('LinCombFig.pdf'  ,'-dpdf', '-bestfit')
     
       
-  end
+  % end
     
