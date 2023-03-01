b5ee825607267eabf4070aaa939595c5b235bd4b
Alain Plattner <alainplattner@gmail.com>
2023-02-28 21:04:19 -0600

    working on leave one out for fixed and for induction

--- LOCV.m
+++ LOCV.m
@@ -1,8 +1,10 @@
-function rmsvals = LOCV(Lmax)
+function rmsvals = LOCV(Lmax,ind,fix)
 
 % Do skipTrack for all orbits, then calculate rms
 rmsvals = nan(1,7);
 for i=1:7
-    rmsvals(i) = skipTrack(Lmax,i);
+    rmsvals(i) = skipTrack(Lmax,i,ind,fix);
 end
-rmsvals(5)=[];
+
+% The following was to test what happens if we skip G28...
+% rmsvals(5)=[];
--- skipTrack.m
+++ skipTrack.m
@@ -1,5 +1,8 @@
-function rmsval = skipTrack(Lmax,wch)
+function rmsval = skipTrack(Lmax,wch,ind,fix)
 % wch is index of track. For example, wch=3 is track 7.
+
+defval('ind',[]) % which induction model
+defval('fix',[]) % the index to fix
 
 rplanet = 2631.2;
 tracks=[1,2,7,8,28,29,101];
@@ -7,20 +10,55 @@ tracks=[1,2,7,8,28,29,101];
 tracksinv = tracks;
 tracksinv(wch) = [];
 
-coefs =  invMoreTracks(Lmax,true,tracksinv,[],[],[]);
+if fix==1
+  coefs = invSkipChoosenCoefSubMoreTracks(Lmax,6,-35,true,tracksinv,[],ind,[]);
+elseif fix==2
+  coefs = invSkipChoosenCoefSubMoreTracks(Lmax,7,24,true,tracksinv,[],ind,[]);
+else
+  coefs =  invMoreTracks(Lmax,true,tracksinv,[],ind,[]);
+end
 cf = coefs(1:(Lmax+1)^2-1)/rplanet;
 
 % Need to put the corresponding BU values back into the coefficients
 % I use the BU values from the paper, Table 2. These are also stored in the
 % coefficients for Lmax2 and Lmax3
 
-LmaxBU = Lmax;
-fullcoefs = load(fullfile('coefs',sprintf('Lmax%d_coefs.txt',LmaxBU)));
-BUvals = fullcoefs((LmaxBU+1)^2:end);
+%LmaxBU = Lmax;
+%fullcoefs = load(fullfile('coefs',sprintf('Lmax%d_coefs.txt',LmaxBU)));
+if fix==1
+  fixname = '_B_i6_v-35' 
+elseif fix==2
+  fixname = '_C_i7_v24';
+else
+  fixname = '';
+end
+
+if strcmp(ind,'high')
+  indname = '_indhigh';
+elseif strcmp(ind,'low')
+  indname = '_indlow';
+elseif strcmp(ind,'simple')
+  indname = '_indsimple';
+else
+  indname = '';
+end
+  
+  
+load(fullfile('coefs',sprintf('Lmax%d%s%s_manycoefs',Lmax,fixname,indname)));
+fullcoefs = mean(cfmat,2);
+BUvals = fullcoefs((Lmax+1)^2:end);
 
 % Replace the resulting track BU values with the stored track BU values.
 coefs = [cf;BUvals];
 
-rmstracks = getRMS(coefs,Lmax);
-rmsval = rmstracks(wch);
+if ind
+  rmstracks = getRMSind(coefs,Lmax,[],ind);
+else
+  rmstracks = getRMS(coefs,Lmax);
+end
 
+try
+rmsval = rmstracks(wch);
+catch
+  keyboard
+  end
